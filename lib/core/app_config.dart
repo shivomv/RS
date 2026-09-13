774eb3e175e90d54fa1547e5e4500ad6be0f6a27
Shivom Verma <oms43711@gmail.com>
2026-09-13 20:51:50 +0530

    Use deployed backend API

--- lib/core/app_config.dart
+++ lib/core/app_config.dart
@@ -1,15 +1,4 @@
-import 'package:flutter/foundation.dart';
-
 class AppConfig {
-  // Use http://10.0.2.2:5000 for Android Emulator
-  // Use http://localhost:5000 for iOS, Web or Desktop
-  static String get baseUrl {
-    if (kIsWeb) {
-      return "http://localhost:5000/api";
-    } else {
-      // Assuming Android emulator. For real device, use machine IP.
-      return "http://10.0.2.2:5000/api";
-    }
-  }
+  static const String baseUrl = "https://rs-gamma-olive.vercel.app/api";
 }
 
